dce90884bafa08825e3d7ab9f94daf4d78b86906
Stefan Gogov <stefangogov@proton.me>
2024-07-09 13:01:25 +0300

    Refactor vendor dashboard to use separate layout and sidebar

--- app/Http/Controllers/Backend/VendorController.php
+++ app/Http/Controllers/Backend/VendorController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 
 class VendorController extends Controller {
     public function dashboard() {
-        return view('vendor.dashboard');
+        return view('vendor.dashboard.dashboard');
     }
 }
