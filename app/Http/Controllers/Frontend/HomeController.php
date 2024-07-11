d61a2d5a942dc0322a610f09c3295e290d819fce
Stefan Gogov <stefangogov@proton.me>
2024-07-11 16:38:32 +0300

    feat: Show slider in the home page

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Slider;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller {
     public function index() {
-        return view('frontend.home.home');
+        // Get all active sliders, ordered by the serial column
+        $sliders = Slider::where('status', 1)->orderBy('serial', 'ASC')->get();
+
+        return view('frontend.home.home', compact('sliders'));
     }
 }
